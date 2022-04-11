ad3044a3fce188d003baa0490c07d6718098f1e0
Makrand <makrandshingare@gmail.com>
2022-04-11 20:36:16 +0530

    Added :JSON for different HTTP Methods

--- src/main/java/com/biz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/biz/greetingapp/controller/GreetingController.java
@@ -1,20 +1,34 @@
 package com.biz.greetingapp.controller;
 
 import com.biz.greetingapp.entity.Greeting;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping("/greeting")
 public class GreetingController {
     private static final String template="Hello, %s";
     private final AtomicLong counter = new AtomicLong();
 
     // Controller for return JSON for get ethods.
-    @GetMapping("greeting")
+
+    @GetMapping("/")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
         return new Greeting(counter.incrementAndGet(),String.format(template,name));
+    }
+
+
+    @RequestMapping("/user")
+    public Greeting greetingUser(@RequestParam(value="firstName") String firstName,
+                                 @RequestParam(value="lastName") String lastName) {
+        return new Greeting(counter.incrementAndGet(),String.format(template, (firstName+" "+lastName)));
+    }
+
+    @PutMapping("/{firstName}")
+    public Greeting greeting(@PathVariable String firstName,
+                             @RequestParam(value = "lastName") String lastName) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, (firstName+" "+lastName)));
     }
 }
